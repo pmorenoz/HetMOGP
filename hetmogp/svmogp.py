Correct gradient typos and get rid of unnecessary lines

hetmogp/svmogp.py
@@ -32,18 +32,15 @@ class SVMOGP(GPy.core.SparseGP):
         # Batch the data
         self.Xmulti_all, self.Ymulti_all = X, Y
         if batch_size is None:
-            self.stochastic = False
             Xmulti_batch, Ymulti_batch = X, Y
         else:
             # Makes a climin slicer to make drawing minibatches much quicker
-            self.stochastic = True
             self.slicer_list = []
             [self.slicer_list.append(draw_mini_slices(Xmulti_task.shape[0], self.batch_size)) for Xmulti_task in self.Xmulti]
             Xmulti_batch, Ymulti_batch = self.new_batch()
             self.Xmulti, self.Ymulti = Xmulti_batch, Ymulti_batch
 
         # Initialize inducing points Z
-        #Z = kmm_init(self.X_all, self.num_inducing)
         self.Xdim = Z.shape[1]
         Z = np.tile(Z,(1,self.num_latent_funcs))
 
@@ -60,8 +57,8 @@ class SVMOGP(GPy.core.SparseGP):
                                   W_list=self.W_list, kappa_list=self.kappa_list)
 
         # Set-up optimization parameters: [Z, m_u, L_u]
-        self.q_u_means = Param('m_u', 5*np.random.randn(self.num_inducing, self.num_latent_funcs) +
-                               np.tile(np.random.randn(1,self.num_latent_funcs),(self.num_inducing,1)))
+        self.q_u_means = Param('m_u', 0*np.random.randn(self.num_inducing, self.num_latent_funcs) +
+                               0*np.tile(np.random.randn(1,self.num_latent_funcs),(self.num_inducing,1)))
         chols = choleskies.triang_to_flat(np.tile(np.eye(self.num_inducing)[None,:,:], (self.num_latent_funcs,1,1)))
         self.q_u_chols = Param('L_u', chols)
 
@@ -71,8 +68,6 @@ class SVMOGP(GPy.core.SparseGP):
         [self.link_parameter(kern_q) for kern_q in kern_list]  # link all kernels
         [self.link_parameter(B_q) for B_q in self.B_list]
 
-        self.vem_step = True # [True=VE-step, False=VM-step]
-        self.ve_count = 0
         self.elbo = np.zeros((1,1))
 
 
@@ -84,83 +79,54 @@ class SVMOGP(GPy.core.SparseGP):
         d_index = self.Y_metadata['d_index'].flatten()
         T = len(self.likelihood.likelihoods_list)
         self.batch_scale = []
-        [self.batch_scale.append(float(self.Xmulti_all[t].shape[0] / self.Xmulti[t].shape[0])) for t in range(T)]
-        self._log_marginal_likelihood, gradients, self.posteriors, _ = self.inference_method.inference(q_u_means=self.q_u_means,
+        [self.batch_scale.append(float(self.Xmulti_all[t].shape[0])/float(self.Xmulti[t].shape[0])) for t in range(T)]
+        self._log_marginal_likelihood, self.gradients, self.posteriors, _ = self.inference_method.inference(q_u_means=self.q_u_means,
                                                                         q_u_chols=self.q_u_chols, X=self.Xmulti, Y=self.Ymulti, Z=self.Z,
                                                                         kern_list=self.kern_list, likelihood=self.likelihood,
                                                                         B_list=self.B_list, Y_metadata=self.Y_metadata, batch_scale=self.batch_scale)
         D = self.likelihood.num_output_functions(self.Y_metadata)
         N = self.X.shape[0]
         M = self.num_inducing
-        _, B_list = util.LCM(input_dim=self.Xdim, output_dim=D, rank=1, kernels_list=self.kern_list, W_list=self.W_list,
-                             kappa_list=self.kappa_list)
+
         Z_grad = np.zeros_like(self.Z.values)
         for q, kern_q in enumerate(self.kern_list):
             # Update the variational parameter gradients:
-            # SVI + VEM
-            if self.stochastic:
-                if self.vem_step:
-                    self.q_u_means[:, q:q + 1].gradient = gradients['dL_dmu_u'][q]
-                    self.q_u_chols[:, q:q + 1].gradient = gradients['dL_dL_u'][q]
-                else:
-                    self.q_u_means[:, q:q+1].gradient = np.zeros(gradients['dL_dmu_u'][q].shape)
-                    self.q_u_chols[:,q:q+1].gradient = np.zeros(gradients['dL_dL_u'][q].shape)
-            else:
-                self.q_u_means[:, q:q + 1].gradient = gradients['dL_dmu_u'][q]
-                self.q_u_chols[:, q:q + 1].gradient = gradients['dL_dL_u'][q]
+
+            self.q_u_means[:, q:q + 1].gradient = self.gradients['dL_dmu_u'][q]
+            self.q_u_chols[:, q:q + 1].gradient = self.gradients['dL_dL_u'][q]
 
             # Update kernel hyperparameters: lengthscale and variance
-            kern_q.update_gradients_full(gradients['dL_dKmm'][q], self.Z[:,q*self.Xdim:q*self.Xdim+self.Xdim])
+            kern_q.update_gradients_full(self.gradients['dL_dKmm'][q], self.Z[:,q*self.Xdim:q*self.Xdim+self.Xdim])
             grad = kern_q.gradient.copy()
 
             # Update kernel hyperparameters: W + kappa
             Kffdiag = []
             KuqF = []
             for d in range(D):
-                Kffdiag.append(gradients['dL_dKdiag'][q][d])
-                KuqF.append(gradients['dL_dKmn'][q][d] * kern_q.K(self.Z[:,q*self.Xdim:q*self.Xdim+self.Xdim], self.Xmulti[f_index[d]]))
+                Kffdiag.append(kern_q.Kdiag(self.Xmulti[f_index[d]]) * self.gradients['dL_dKdiag'][q][d])
+                KuqF.append(kern_q.K(self.Z[:,q*self.Xdim:q*self.Xdim+self.Xdim], self.Xmulti[f_index[d]]) * self.gradients['dL_dKmn'][q][d])
 
             util.update_gradients_diag(self.B_list[q], Kffdiag)
             Bgrad = self.B_list[q].gradient.copy()
             util.update_gradients_Kmn(self.B_list[q], KuqF, D)
             Bgrad += self.B_list[q].gradient.copy()
-            # SVI + VEM
-            if self.stochastic:
-                if self.vem_step:
-                    self.B_list[q].gradient = np.zeros(Bgrad.shape)
-                else:
-                    self.B_list[q].gradient = Bgrad
-            else:
-                self.B_list[q].gradient = Bgrad
+
+            self.B_list[q].gradient = Bgrad
 
             for d in range(self.likelihood.num_output_functions(self.Y_metadata)):
-                kern_q.update_gradients_full(gradients['dL_dKmn'][q][d], self.Z[:,q*self.Xdim:q*self.Xdim+self.Xdim], self.Xmulti[f_index[d]])
-                grad += B_list[q].W[d]*kern_q.gradient.copy()
-                kern_q.update_gradients_diag(gradients['dL_dKdiag'][q][d], self.Xmulti[f_index[d]])
-                grad += B_list[q].B[d,d] * kern_q.gradient.copy()
-                # SVI + VEM
-                if self.stochastic:
-                    if self.vem_step:
-                        kern_q.gradient = np.zeros(grad.shape)
-                    else:
-                        kern_q.gradient = grad
-                else:
-                    kern_q.gradient = grad
+                kern_q.update_gradients_full(self.B_list[q].W[d] * self.gradients['dL_dKmn'][q][d],self.Z[:, q * self.Xdim:q * self.Xdim + self.Xdim],self.Xmulti[f_index[d]])
+                grad += kern_q.gradient.copy()
+                kern_q.update_gradients_diag(self.B_list[q].B[d,d] *self.gradients['dL_dKdiag'][q][d], self.Xmulti[f_index[d]])
+                grad += kern_q.gradient.copy()  # Juan wrote this line
+
+            kern_q.gradient = grad
 
             if not self.Z.is_fixed:
-                Z_grad[:,q*self.Xdim:q*self.Xdim+self.Xdim] += kern_q.gradients_X(gradients['dL_dKmm'][q], self.Z[:,q*self.Xdim:q*self.Xdim+self.Xdim])
+                Z_grad[:,q*self.Xdim:q*self.Xdim+self.Xdim] += kern_q.gradients_X(self.gradients['dL_dKmm'][q], self.Z[:,q*self.Xdim:q*self.Xdim+self.Xdim]).copy()
                 for d in range(self.likelihood.num_output_functions(self.Y_metadata)):
-                    Z_grad[:,q*self.Xdim:q*self.Xdim+self.Xdim] += B_list[q].W[d]*kern_q.gradients_X(gradients['dL_dKmn'][q][d], self.Z[:,q*self.Xdim:q*self.Xdim+self.Xdim], self.Xmulti[f_index[d]])
+                    Z_grad[:,q*self.Xdim:q*self.Xdim+self.Xdim]+= self.B_list[q].W[d]*kern_q.gradients_X(self.gradients['dL_dKmn'][q][d], self.Z[:, q * self.Xdim:q * self.Xdim + self.Xdim],self.Xmulti[f_index[d]]).copy()
 
-        if not self.Z.is_fixed:
-            # SVI + VEM
-            if self.stochastic:
-                if self.vem_step:
-                    self.Z.gradient[:] = np.zeros(Z_grad.shape)
-                else:
-                    self.Z.gradient[:] = Z_grad
-            else:
-                self.Z.gradient[:] = Z_grad
+        self.Z.gradient[:] = Z_grad
 
     def set_data(self, X, Y):
         """
@@ -185,19 +151,11 @@ class SVMOGP(GPy.core.SparseGP):
     def stochastic_grad(self, parameters):
         self.set_data(*self.new_batch())
         stochastic_gradients = self._grads(parameters)
-        if self.vem_step:
-            if self.ve_count > 2:
-                self.ve_count = 0
-                self.vem_step = False
-            else:
-                self.ve_count += 1
-        else:
-            self.vem_step = True
         return stochastic_gradients
 
     def callback(self, i, max_iter, verbose=True, verbose_plot=False):
         ll = self.log_likelihood()
-        self.elbo[i['n_iter']-1,0] =  self.log_likelihood()[0]
+        self.elbo[i['n_iter'],0] =  self.log_likelihood()[0]
         if verbose:
             if i['n_iter']%50 ==0:
                 print('svi - iteration '+str(i['n_iter'])+'/'+str(int(max_iter)))